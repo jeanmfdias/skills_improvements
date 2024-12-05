c149b30872b22a33bc743a9cbb3a280c140337b1
jeanmfdias <jeanmfdias@gmail.com>
2024-12-05 12:12:30 -0300

    Challenge Alura to create Study class

--- HelloWorld/src/Profile.java
+++ HelloWorld/src/Profile.java
@@ -1,6 +1,5 @@
 public class Profile {
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         String name = "Jean Dias";
         System.out.printf("Hello %s \n", name);
         System.out.println("Tudo bem?");
---
+++ HelloWorld/src/Study.java
@@ -0,0 +1,6 @@
+public class Study {
+    public static void main(String[] args) {
+        System.out.println("I'm remember from my college classes");
+        System.out.println("But the Java has had many improvements");
+    }
+}
