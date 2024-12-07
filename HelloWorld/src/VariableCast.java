d62fdb24b0f03ee5198329aea1033f4481841ee4
jeanmfdias <jeanmfdias@gmail.com>
2024-12-07 05:15:10 -0300

    Casting of numeric variables

---
+++ HelloWorld/src/VariableCast.java
@@ -0,0 +1,8 @@
+public class VariableCast {
+    public static void main(String[] args) {
+        double num = 10.4;
+        int newNum = (int) num;
+        System.out.println(num);
+        System.out.println(newNum);
+    }
+}
