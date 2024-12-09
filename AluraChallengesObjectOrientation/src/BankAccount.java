b31f69e273ec408bc1c7eb954ec04220dd80d44f
jeanmfdias <jeanmfdias@gmail.com>
2024-12-09 12:44:49 -0300

    Create challenge to class bank account

---
+++ AluraChallengesObjectOrientation/src/BankAccount.java
@@ -0,0 +1,21 @@
+public class BankAccount {
+    private int number;
+    private double amount;
+    public String owner;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
